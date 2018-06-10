65b35fbeb060841dc9c09f2443eda4491175c081
jacobsilver2 <jacobsilver2@mac.com>
2018-06-10 13:23:09 -0400

    Done.

--- src/components/Pikachu.js
+++ src/components/Pikachu.js
@@ -9,6 +9,11 @@ class Pikachu extends React.Component {
       size: 100
     };
   }
+
+  componentDidUpdate() {
+      this.resizePikachu();
+
+  }
   
   resizePikachu = () => {
     const pikachu = document.getElementById("pikachu");
